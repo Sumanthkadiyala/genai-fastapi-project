a6a70dda8a250cc3519424f94573870e7d817d52
SumanthKadiyala <sumanthkadiyala7@gmail.com>
2026-05-07 17:01:26 +0530

    Initial FastAPI setup

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "Hello World"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
